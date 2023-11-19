668e18149619327b771946ab3329adda49ac410a
ACH RIZAL <achrizal15@gmail.com>
2023-11-19 12:37:52 +0700

    WA

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-Route::post("/wa", [WaHooks::class, "index"]);
-Route::get("/wa", [WaHooks::class, "index"]);
+Route::match(['get', 'post'], '/wa', [WaHooks::class, "index"]);
